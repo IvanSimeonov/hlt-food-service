2c31b329b76f30a7310624f29c263d71293eac05
Ivan Simeonov <ivan.simeonov@frox.ch>
2022-07-16 19:01:13 +0300

    Initial commit

---
+++ src/main/java/com/ivnsim/hltfoodservice/HltFoodServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ivnsim.hltfoodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HltFoodServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HltFoodServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivnsim/hltfoodservice/HltFoodServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivnsim.hltfoodservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HltFoodServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
